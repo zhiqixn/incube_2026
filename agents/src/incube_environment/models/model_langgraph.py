8572ef1b92e6fecea7e40d486aa2755196f709a7
Brandon Yeow <brandonywl@gmail.com>
2026-04-13 22:39:00 +0800

    Updated model invocation to default on .env

--- agents/src/incube_environment/models/model_langgraph.py
+++ agents/src/incube_environment/models/model_langgraph.py
@@ -2,7 +2,7 @@ import os
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
-    base_url="http://192.168.100.1:5000/v1",
-    api_key="not-needed",
-    model="gemma-4-26B-A4B-it",
+    base_url=os.getenv("MODEL_ENDPOINT") or "http://192.168.100.1:5000/v1",
+    api_key=os.getenv("MODEL_API_KEY") or "not-needed",
+    model=os.getenv("MODEL_NAME") or "gemma-4-26B-A4B-it",
 )
